1dfa8d6904858bc434a2b31307ca780571972660
Akshay k <165384973+itsakshay321@users.noreply.github.com>
2025-05-19 21:18:17 +0530

    Update db_config.js

--- Blockchain-Realty---Modernizing-Real-Estate-main/pro-main/Server/backend/Config/db_config.js
+++ Blockchain-Realty---Modernizing-Real-Estate-main/pro-main/Server/backend/Config/db_config.js
@@ -4,5 +4,5 @@ module.exports = {
   NEXMO_API_KEY: "API_KEY",
   NEXMO_API_SECRET: "SECRET_KEY",
   NEXMO_FROM_NUMBER: "NUMBER",
-  MongoURI: 'mongodb+srv://abhinaathi:abhin123@cluster0.flgpo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  MongoURI: 'mongodb+srv://a+++++++0.flgpo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 }
